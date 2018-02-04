648721a022e3fa8a05405963e2cb03aee68c34eb
sagarkaniche <sagarkaniche007@gmail.com>
2018-02-04 18:33:37 +0530

    Test Commit

--- app/src/main/java/com/kanpirah/hkk/marblesapp1/MainActivity.java
+++ app/src/main/java/com/kanpirah/hkk/marblesapp1/MainActivity.java
@@ -7,6 +7,8 @@ public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        int i = 0;
+        i++;
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
